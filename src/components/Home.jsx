762766f184a4f8f23bd507b157621eab9617183d
Christopher Sanders <astericky@gmail.com>
2016-12-26 10:34:35 -0500

    Update navigation and sidebar styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,9 @@ import React, { PropTypes } from 'react'
 import styles from './Home.css'
 
 const Content = (props) => (
-  <main className={styles["tg-content"]}>
-
+  <main className={styles["tg-content-container"]}>
+    <section className={styles["content"]}></section>
+    <aside className={styles["sidebar"]}></aside>
   </main>
 )
 
