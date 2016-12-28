347cfae8457cae8b0ed2087d7405f6c2df6d958b
Christopher Sanders <astericky@gmail.com>
2016-12-28 16:24:39 -0500

    Add article excerpt

---
+++ src/components/ArticleExcerpt.jsx
@@ -0,0 +1,23 @@
+import React, { PropTypes } from 'react'
+
+import styles from './ArticleExcerpt.css'
+
+const ArticleExcerpt = (props) => {
+  const { title, date, tags, post } = props
+  let postText = post.split('\n').map((p, index) => <p key={index}>{p.trim()}</p>)
+
+  return (
+    <article className={styles['article-excerpt']}>
+      <header>
+        <h3>{title}</h3>
+        <datetime>{date}</datetime>
+      </header>
+      <div className="text">{postText}</div>
+      <footer>
+        <span className="tags">{tags}</span>
+      </footer>
+    </article>
+  )
+}
+
+export default ArticleExcerpt
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,47 @@
 import React, { PropTypes } from 'react'
 
 import styles from './Home.css'
+import ArticleExcerpt from './ArticleExcerpt.jsx'
 
-const Content = (props) => (
-  <main className={styles["tg-content-container"]}>
-    <section className={styles["content"]}></section>
-    <aside className={styles["sidebar"]}></aside>
-  </main>
-)
+function renderArticles(articles = [
+  {
+    title: "Fake Title",
+    date: "Jan 1, 2017",
+    tags: "stuff, stuff, stuff, stuff",
+    post: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+  },
+  {
+    title: "Fake Title",
+    date: "Jan 1, 2017",
+    tags: "stuff, stuff, stuff, stuff",
+    post: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+  },
+  {
+    title: "Fake Title",
+    date: "Jan 1, 2017",
+    tags: "stuff, stuff, stuff, stuff",
+    post: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+  },
+]) {
+  return articles.map((article, index) => (
+    <ArticleExcerpt key={index} {...article} />
+  ))
+}
+
+class Content extends React.Component {
+  render () {
+    return (
+      <main className={styles["tg-content-container"]}>
+        <section className={styles["content"]}>
+          <article className={styles["featured-article"]}></article>
+          {renderArticles()}
+        </section>
+        <aside className={styles["sidebar"]}></aside>
+      </main>
+    )
+  }
+}
 
 export default Content
